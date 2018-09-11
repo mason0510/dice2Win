e56f3c98648e51bdfc4225ed8d5358febda55de5
e <zhangxiaocong_2011@yeah.net>
2018-09-11 21:01:38 +0800

    提交了dice2Win代码

---
+++ compile.js
@@ -0,0 +1,12 @@
+//编译的智能合约的脚本
+const path = require('path');
+const fs = require('fs');
+const solc = require('solc');
+
+const srcpath = path.resolve(__dirname,'contracts','Lottery.sol');
+const source = fs.readFileSync(srcpath,'utf-8');
+
+const result = solc.compile(source,1);
+console.log("打印数据"+result.toLocaleString());
+
+module.exports = result.contracts[':Lottery'];
